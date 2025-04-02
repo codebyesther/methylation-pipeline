6786af9151b5edbe4214ec3a38de1ee5e105e058
codebyesther <estherchoi.rok@gmail.com>
2025-04-02 12:33:17 -0400

    Update bubbleplot_generator_local_unsquished_linearlegend_v6.py

--- scripts/locus/bubbleplot_generator_local_unsquished_linearlegend_v6.py
+++ scripts/locus/bubbleplot_generator_local_unsquished_linearlegend_v6.py
@@ -188,7 +188,7 @@ for patient in tqdm(collapsed.columns.levels[0], desc="Generating bubble plots p
         bubble_sizes = [size**0.5 * 50 for size in sizes]
 
         # Tightly spaced vertical positions
-        positions = np.linspace(0.18, 0.65, len(sizes))
+        positions = np.linspace(0.18, 0.4, len(sizes))
 
         # Manually draw the legend using scatter and text
         for size, pos in zip(sizes, positions):
@@ -272,7 +272,7 @@ for chrom in tqdm(coords_df["Chr"].unique(), desc="Generating bubble plots per c
     bubble_sizes = [size**0.5 * 50 for size in sizes]
 
     # Tightly spaced vertical positions
-    positions = np.linspace(0.18, 0.65, len(sizes))
+    positions = np.linspace(0.18, 0.4, len(sizes))
 
     # Manually draw the legend using scatter and text
     for size, pos in zip(sizes, positions):
