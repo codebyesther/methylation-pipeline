4777a7385b0c5054ad2ebfa6a6095973fdf546d3
codebyesther <estherchoi.rok@gmail.com>
2025-04-04 20:36:41 -0400

    Update top10dm-plots.py

--- scripts/locus/top10dm-plots.py
+++ scripts/locus/top10dm-plots.py
@@ -105,7 +105,9 @@ for fname, df in methylation_dfs.items():
         sns.barplot(data=df.head(10), x="Avg_Delta", y="CpG_Island", color='darkblue')
         plt.xlabel("Avg Change in Scaled Methylated Fragment Count Ratio")
         plt.axvline(0, color="gray", linestyle="--")
-        plt.title(title, loc='left')  # Move the title to the left
+        # Remove the default title and use plt.text to position it manually
+        plt.title("")  # Remove the default title
+        plt.text(plt.gca().get_position().xmin, 1.02, title, ha='left', va='bottom', transform=plt.gca().transAxes, fontsize=12)
         plt.tight_layout()
         plot_path = os.path.join(args.outdir, filename)
         plt.savefig(plot_path)
